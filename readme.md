94658978fcb32887338d965da89d38222f3386b5
Tetration <rafael.august.b@gmail.com>
2020-06-08 00:38:07 -0300

    Added option to convert texts found to mp3 audio by using Google Speech recognition software

--- readme.md
+++ readme.md
@@ -3,17 +3,34 @@
 -
 AskTheBible is a python script that takes advantage of Selenium's powerful capabilities to search about Biblical verses in https://www.openbible.info/
 
+## INSTALLATION
+
+```
+To make the installation process easy simply open this folder using your cmd, powershell or terminal and then type the following command below
+python -m pip install -r requirements.txt
+
+Afterwards make sure to Download Chromedriver and place it inside the folder where this script is located
+       You can download it by going to https://chromedriver.chromium.org/downloads
+       
+       Warning: make sure to download the versions corresponding to the current Chrome you have installed on your computer.
+       In order to check which version of chrome you have inside your machine launch your chrome, then click on the Menu icon in the upper right corner of the screen,
+       then click on Help, and then lastly click on About Google Chrome, as soon as you click on it a screen showing your current Chrome version number will appear. 
+       Now, that you know which version number of chrome you have, all you have to do now is download the corresponding version at chromedriver.
+       Example: this python script is currently located inside the folder src, so as soon as you finish dowloading the corresponding chromedriver version for your chrome.
+       You need to place it inside the "src" folder where this script is located at. Only then this script will run as intended.
+
+```
 **Requirements:**
 -
 1. Python 3 installed on your computer
-2. Selenium installed on your python through pip command
+2. Selenium, gTTs, secrets and play sound installed on your python through pip command
 3. ChromeDrive in the folder where this script is located at
 4. Chrome installed in your OS
 
 
 **Future plans**
--
-- Ability to save bible verses you found in a pdf file.
+
+- Ability to save bible verses you found to a pdf file.
 - Ability to search about biblical verses in other versions of the bible example: KJV, NKJV and so on.
 
 **Warning:**
--- src/askTheBible.py
+++ src/askTheBible.py
@@ -18,10 +18,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from operator import methodcaller
-
+from gtts import gTTS 
+from playsound import playsound
 
 versesList = [None]
-searchList = ["holy trinity","trinity", "father", "mother", "water","worship","sea", "fake friends","fish","jesus", "jealous", "envy","forthcoming","struggles", "challenges", "witness","bearing with one another", "boredom","dreams", "daugther", "darkness","prosperity","eternity", "book of life", "Lazarus","love","life", "light","marriage", "gold", "humility", "humble", "Kingdom","Kingdom come","wisdom", "judgement", "judgemental", "patience", "destiny", "sheep","path", "rewards", "enemies", "betrayal", "traitor", "hardships", "overcoming hardships", "peace", "forgiving", "temptation" , "christ", "devil", "demon", "persist","purgatory", "heaven", "hell", "damnation", "child", "wine", "food", "shelter", "future", "present", "past", "distress","disease", "diseases", "cure", "heal", "holy spirit", "defile", "repent", "son", "Solomon", "sin", "apocalypse", "slave", "free", "freedom", "plague", "egypt", "romans", "greek", "false prophet", "sorcery", "serpent", "beast", "dragon", "rage","fallen angel", "avenger", "authorities", "government", "tax collector"]
+searchList = ["holy trinity","trinity", "father", "mother", "water","worship","sea", "sloth", "fake friends","fish", "focus","jesus", "jew","jealous", "envy","forthcoming","struggles", "challenges", "witness","bearing with one another", "boredom", "being hated", "dreams", "daugther", "darkness","prosperity","eternity", "book of life", "Lazarus","love","life", "light","marriage", "gold", "humility", "humble", "Kingdom","Kingdom come","wisdom", "judgement", "judgemental", "patience", "pride","destiny", "sheep","path", "rewards", "right girl","enemies", "betrayal", "traitor", "hardships", "hate", "overcoming hardships", "peace", "redemption","forgiving", "temptation" , "christ", "devil", "demon", "persist","purgatory", "heaven", "hell", "damnation", "child", "wine", "food", "shelter", "future", "present", "past", "distress","disease", "diseases", "cure", "heal", "holy spirit", "defile", "repent", "son", "Solomon", "sin", "apocalypse", "slave", "free", "freedom", "plague", "egypt", "romans", "greek", "hebrew", "false prophet", "sorcery", "witchcraft", "serpent", "beast", "dragon", "rage","fallen angel", "flesh", "avenger", "authorities", "government", "governor","tax collector", "end of the world", "end of times"]
 
 class Biblical_verse:
     def __init__(self, verse_location, bible_version, verse_content):
@@ -35,7 +36,18 @@ class Biblical_verse:
     def returnStringOfEntireVerse(self):
         entireVerse= self.verse_location+"\n"+self.bible_version+"\n"+self.verse_content+"\n"
         return entireVerse
+    def Get_Verse_for_Audio_Conversion(self):
+        everything = self.verse_location + " " + self.verse_content 
+        return everything
 
+def checkIfFileAlreadyExists(Filename):
+    #checks if file exists in current directory this python script is located at
+    tf=os.path.exists(Filename)
+    return tf
+
+
+def user_hasRequiredModules():
+    print("Checking if user has required modules")
 
 def remove_html_tags(text):
     """Remove html tags from a string"""
@@ -74,6 +86,9 @@ def GetBiblicalVerses(driver, userQuestion):
 
     versesList.pop(0) 
     for create_verse in versesList:
+        index= versesList.index(create_verse)
+        print("Position", index,sep = " ")
+        print("")
         create_verse.show_verse()
         print("")
     driver.quit()
@@ -110,23 +125,36 @@ def SearchAboutUserQuestionInTheBible(userQuestion):
     #re.match(r'^[-+]?([1-9]\d*|0)$', found)
     print("Found", found, "Bible verses about", userQuestion ,sep=" ")
     GetBiblicalVerses(driver,userQuestion)
+    DecisionText(userQuestion)
+    DecisionAudio(userQuestion)
+
+
+def DecisionText(userQuestion):
     saveOrNot = saveTextMenu(userQuestion)
     if saveOrNot=='1' or saveOrNot =='2':
         saveFilesToTXTOrPDF(saveOrNot, userQuestion)
+
+def DecisionAudio(userQuestion):
+    saveAudioOrNot=saveAudioMenu(userQuestion)
+    if saveAudioOrNot=='1':
+        convertSpecificTextVerseToAudio(userQuestion)
+    elif saveAudioOrNot == '2':
+        convertAllTextVersesToAudio(userQuestion)
+        
 
 def saveTextMenu(userSearch):
     userInput=""
     corretOption= False
     while corretOption==False:
-        print("Would you like to save the biblical verses that we found about ", userSearch,"?" ,sep="")
-        print("1- Save the biblical verses in a txt file")
-        #print("2- Surprise me")
+        print("Would you like to also save the biblical verses that we found about ", userSearch,"?" ,sep="")
+        print("1- Save the Biblical verses in a txt file")
+        print("2- Save the Biblical verses in a PDF file")
         print("3- Don't save")
         userInput=input();
         if userInput=='1':
             corretOption=True
             break
-        elif userInput=='2':
+        elif userInput=="2":
             print("For now, option", userInput, "does nothing, come back later to check if the developer implements the option to save to pdf yet", sep=" ")
             corretOption=True
             break
@@ -137,7 +165,102 @@ def saveTextMenu(userSearch):
             print("Invalid choice, please pick one of the options avaliable")
     return userInput
 
+def saveAudioMenu(userSearch):
+    userAnswer=""
+    userAnswered=False
+    while userAnswered==False:
+        print("Convert Text to Speech Menu")
+        print("Would you like to also Convert the biblical text that we found about", userSearch,"to Speech(audio file)?" ,sep=" ")
+        print("0 - List all Biblical verses found about", userSearch, " to know their position in our table",sep=" ")
+        print("1 - Convert a specific Biblical Verse to an Audio File (need to know its position number in our table)")
+        print("2 - Convert all Biblical Texts found about",  userSearch, "to an Audio File ", "?", sep=" ")
+        print("3 - Exit AskTheBible without converting verses about",  userSearch, "to an Audio File ", "?", sep=" ")
+        userAnswer=input()
+        if userAnswer=='0':
+            print("List all verses about",  userSearch,"showing their position number in our table..",sep=" ")
+        elif userAnswer=="1":
+            userAnswered=True
+            break
+        elif userAnswer=="2":
+            userAnswered=True
+            break
+        elif userAnswer=='3':
+            userAnswered=True
+            break
+        else:
+            print("Invalid choice, please pick one of the options avaliable")
+        #print("Going back to SaveTextMenu....")
+    return userAnswer
+
+def convertAllTextVersesToAudio(userQuestion):
+    if not versesList:
+        print("Error can't convert any text the list seems empty")
+        print("The program will now exit")
+        exit()
+
+    print("Converting all Biblical Texts about", userQuestion,"to an Audio File", sep=" ")
+    mytext = ' Philippians 4:13 \n ESV \n I can do all things through him who strengthens me.'
+    language = 'en'
+    listSize=len(versesList)
+    for create_verse in versesList:
+        index= versesList.index(create_verse)
+        #print("Position", index,sep = " ")
+        mytext=create_verse.Get_Verse_for_Audio_Conversion()
+        myobj = gTTS(text=mytext, lang=language, slow=False)
+        oldname= create_verse.verse_location
+        oldname = oldname.replace(":", "_")
+        filename=oldname+".mp3"
+        myobj.save(filename) 
+        filelocation= os.path.dirname(os.path.realpath(filename))
+        print("Created mp3 file for verse", create_verse.verse_location,"which can be located at", filelocation,"under the name of", filename, sep=" ")
+        print(versesList.index(create_verse), "out of", listSize,"verses to convert to mp3 left", sep=" ")
+    print("")
+    print("Finished converting all Biblical verses about", userQuestion, "to mp3 files", sep= " ")
+     
+    
+def convertSpecificTextVerseToAudio(userQuestion):
+    if not versesList:
+        print("Error cant convert any text the list seems empty")
+        print("The program will now exit")
+        exit()
+    for create_verse in versesList:
+        index= versesList.index(create_verse)
+        print("Position", index,sep = " ")
+        print("")
+        create_verse.show_verse()
+        print("")
+
+    print("Which verse would you like to pick? Please, type its position on the list as shown in the list above")
+    print(len(versesList))
+    userAnswer=""
+    userAnswered=False
+    listSize=len(versesList)
+    shift = -1
+    while 0 > shift or listSize < shift and userAnswered==False:
+        try:
+            print("Please enter the location of the verse you which to convert to mp3 (0 - ", len(versesList), ") : ",sep="")
+            shift = int(input())
+            userAnswer=True
+        except ValueError:
+            print ("Error: Value typed is out of range, please type a number in the range of biblical verses found about", userQuestion, sep=" ")
+    mytext = ''
+    language = 'en'
+    verse= versesList[shift]
+    #print("Position", index,sep = " ")
+    mytext=verse.Get_Verse_for_Audio_Conversion()
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    oldname= verse.verse_location
+    oldname = oldname.replace(":", "_")
+    filename=oldname+".mp3"
+    myobj.save(filename) 
+    filelocation= os.path.dirname(os.path.realpath(filename))
+    print("Created mp3 file for verse", verse.verse_location,"which can be located at", filelocation, "under the name of", filename, sep=" ")
+
 def saveFilesToTXTOrPDF(userChoice, userQuestion):
+    if not versesList:
+        print("Error cant convert any text the list seems empty")
+        print("The program will now exit")
+        exit()
     print("Saving log in")
     if userChoice=='1':
         print("Saving verses about", userQuestion,"to a txt file...", sep=" ")
@@ -149,7 +272,7 @@ def saveFilesToTXTOrPDF(userChoice, userQuestion):
         filelocation= os.path.dirname(os.path.realpath(filename))
         print("The file can be found at:",filelocation,sep=" ")
     elif userChoice =='2':
-        print("Saving verses about",  userQuestion,"to a PDF file...", sep=" ")
+        print("Saving verses about",  userQuestion, "to a PDF file...", sep=" ")
         print("This feature is not ready yet. Come back later, to check if the developer has already implemented the option to save to a pdf file")
 
 def WhatToDo():
@@ -162,7 +285,7 @@ def WhatToDo():
         randomPick=secrets.choice(searchList)
         SearchAboutUserQuestionInTheBible(randomPick)
     elif option =='3':
-        print("The program will now exit")
+        print("Exiting the program")
 
 def menu():
     userInput=""
@@ -186,11 +309,20 @@ def menu():
             print("Invalid choice, please pick one of the options avaliable")
     return userInput
 
+def startProgram():
+    if(checkIfFileAlreadyExists("chromedriver.exe")):
+        WhatToDo()
+    else:
+        print("Error, couldn't find chromedriver.exe")
+        print("It looks like you haven't downloaded and placed chromedriver executable yet in the folder where this script is located")
+        print("Please, download the right chromedriver version corresponding to the current version you have installed of chrome browser and place it")
+        print("You can download chromedriver at: https://chromedriver.chromium.org/downloads")
+        print("Once you do this try to run this script again after you have done these steps.")
 
 def main():
         print("Welcome to AskTheBible!")
-        WhatToDo()
-        print("Thanks for using AskTheBible")
+        startProgram()
+        print("Thanks for using AskTheBible and remember, the devil finds work for idle hands. So, keep yourself busy!")
         print("The program will now exit")
         exit()
 
